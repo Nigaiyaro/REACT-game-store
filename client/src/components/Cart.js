a3e65bd0c1c5a5a1df41ab0e6966b52cba17e4ec
niko.valitalo <niko.valitalo@hotmail.com>
2021-03-04 16:34:14 +0200

    functionality of cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -5,24 +5,67 @@ const Cart = ({ cart, setCart }) => {
     // INSTANCIATING TOTALSUM AS 0
     let totalSum = 0;
 
+    console.log("cart");
+    console.log(cart);
+
     // FOR EVERY ITEM IN THE CART, TAKE PRICE AND SUM
     cart.forEach(game => totalSum += game.price)
 
-    // DELETE PRODUCT (with filter)
-    const deleteProduct = (index) => {
-        setCart(cart.filter((game2, i) => index !== i)); // CURRENT BUG, REMOVES ALL ENTRIES WITH GIVEN CONTENT
-    }
+    const idCount = cart.map(game => game.id);
+    console.log("idCount");
+    console.log(idCount);
 
+    const counts = idCount.reduce((acc, value) => ({
+        ...acc,
+        [value]: (acc[value] || 0) + 1
+     }), {});
+
+     console.log("cart");
+     console.log(cart);
+
+     console.log("counts");
+     console.log(counts);
+
+     let sortedArray = [...cart];
+
+     sortedArray.sort((a, b) => a.id - b.id); // SORTS THE CART
+     const uniquedArray = [...new Set(sortedArray.map(id => id))] // MAKES IT WITH ONLY UNIQUE VALUES
+
+     console.log("uniquedArray");
+     console.log(uniquedArray);
+
+    /*
+
+    const genres = [...new Set(games.map(game => game.genre))]; // takes only unique entries with "...new Set"
+
+    else if (selectedSorting === "ascending-price") { // ASCENDING PRICE
+        sortedGames = ([...filteredGames].sort((a, b) => b.price - a.price));
+        console.log("asc price check");
+    } 
+
+    */
+
+     console.log(sortedArray);
+
+    // DELETE PRODUCT
+    const deleteProduct = (id) => {
+        console.log("sesese");
+        const index = cart.findIndex(game => game.id === id);
+        setCart(cart.filter((game2, i) => index !== i));
+
+        // setCart(cart.filter((game2, i) => index !== i));
+    }
 
     // ----- RETURN SECTION -----
     return (
         <div style={{ display: "flex", flexDirection: "column", width: "30rem", margin: "auto" }}>
         <div style={{ marginBottom: "3rem", textAlign: "center"}}>Shopping cart</div>
-            {cart.map((game, i) =>
-                <div key={i} style={{ display: "flex", marginBottom: "10px", justifyContent: "space-between", }}>
+            {uniquedArray.map((game, i) =>
+                <div key={i} style={{ display: "flex", marginBottom: "10px", justifyContent: "space-between" }}>
+                    {counts[game.id]}
                     <div style={{ paddingRight: "1rem" }}>{`${game.name} ${game.price}€`}</div>
 
-                    <div><button onClick={() => deleteProduct(i)}>
+                    <div><button onClick={() => deleteProduct(game.id)}>
                         Remove from cart
                     </button>
 
@@ -30,7 +73,12 @@ const Cart = ({ cart, setCart }) => {
                 </div>
             )}
 
+        {cart.length ?
         <div style={{ marginTop: "2rem" }}>Total price: {totalSum}<button>To checkout</button></div>
+        :
+        <div>Your cart is empty!</div>
+        }
+        
 
         </div>
     )
