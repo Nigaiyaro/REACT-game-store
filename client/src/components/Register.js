b786cbda3f05c0f964121e62fd88c8694667ee04
niko.valitalo <niko.valitalo@hotmail.com>
2021-04-12 15:42:07 +0300

    fiddling with register + server

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -7,18 +7,28 @@ const Register = () => {
     // AUTHENTICATION STATES
     const [registerUsername, setRegisterUsername] = useState("");
     const [registerPassword, setRegisterPassword] = useState("");
+    const [registered, setRegistered] = useState(false);
 
     const register = () => {
-        console.log(registerUsername);
         Axios({
             method: "POST",
             data: {
                 username: registerUsername,
                 password: registerPassword
             },
-            url: "http://localhost:3001/accounts"
+            url: "http://localhost:3001/accounts",
+            credentials: "true"
         }).then((res) => console.log(res))
+        setRegistered(true);
     }
+
+    // const login = () => {
+    //     Axios({
+    //         method: "POST",
+    //         url: "http://localhost:3001/accounts",
+    //         credentials: "true"
+    //     }).then((res) => console.log(res))
+    // }
 
     const styles = {
         flexItem: {
@@ -27,32 +37,39 @@ const Register = () => {
     }
 
     return (
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "10vw" }}>
-            <div style={{ display: "flex", flexDirection: "column", padding: "4vw", border: "2px solid black" }}>
+        <>
 
-                <div>username:</div>
+            {(registered === false) ?
+                <div style={{ display: "flex", justifyContent: "center", marginTop: "10vw" }}>
+                    <div style={{ display: "flex", flexDirection: "column", padding: "4vw", border: "2px solid black" }}>
 
-                <div style={styles.flexItem}>
-                <input
-                    placeholder="username"
-                    onChange={(e) => setRegisterUsername(e.target.value)}
-                />
+                        <div>username:</div>
+
+                        <div style={styles.flexItem}>
+                            <input
+                                placeholder="username"
+                                onChange={(e) => setRegisterUsername(e.target.value)}
+                            />
+                        </div>
+
+
+                        <div>password:</div>
+
+                        <div style={styles.flexItem}>
+                            <input
+                                placeholder="password"
+                                onChange={(e) => setRegisterPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <Button style={{ padding: "0.5rem", marginTop: "2vw" }} onClick={register}>Submit</Button>
+
+                    </div>
                 </div>
+                : <div>Your account has been registered! You will be redirected back to main page and you can proceed to login. (WIP)</div>
+            }
 
-
-                <div>password:</div>
-
-                <div style={styles.flexItem}>
-                <input
-                    placeholder="password"
-                    onChange={(e) => setRegisterPassword(e.target.value)}
-                />
-                </div>
-
-                <Button style={{ padding: "0.5rem", marginTop: "2vw" }} onClick={register}>Submit</Button>
-
-            </div>
-        </div>
+        </>
     )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,12 +1,58 @@
 import jsonServer from "json-server";
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import session from 'express-session';
 
+import User from './user';
+
+const app = express();
+
+// MIDDLEWARES
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// OG
 server.use(middlewares);
 server.use(router);
 
+// NEW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({
+    origin: "http://localhost:3001",
+    credentials: true
+}))
+
+app.use(session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(cookieParser("secretcode"))
+
+// ROUTES
+app.post("/login", (req, res) => {
+    console.log(reg.body);
+});
+
+app.post("/register", (req, res) => {
+    User.findOne({username: req.body.username}, (err,doc) => {
+        if (err) throw err;
+        if (doc) res.send("User already exists.");
+        if (!doc) {
+            res.send("User created.");
+        }
+    });
+    // console.log(reg.body);
+});
+
+app.get("/user", (req, res) => {});
+
+// Server start
 server.listen(3001, () => {
     console.log("JSON server is running");
 });
